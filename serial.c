500a886639ef163455b2976f4915a9c41e8e6a66
Budgetts Frog <budgettsfrog@protonmail.com>
2018-08-23 17:06:28 -0700

    implemented the python console compatible bootloader version

--- serial.c
+++ serial.c
@@ -1,21 +1,32 @@
 #include <serial.h>
 #include <extFlash.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define SEND_ACK UART_write( '1' )
-#define SEND_NACK UART_write( '0' )
+uint8_t serialBuff[SERIAL_BUFF_SIZE];
+uint8_t serialIndex = 0;
+uint8_t serialLength = 0;
+uint8_t numSerialLengthBytes = 0;
+uint8_t serialLengthisSet = 0;
+char    ack[] = {'~', 'O', '?', 'P'};
 
-uint8_t  ping[256];
-uint8_t  pong[256];
-uint8_t  usePong = 0;
-uint8_t  writeTriggered = 0;
-uint16_t ndx = 0;
+uint8_t  flashComplete = 0;
+uint8_t  flashBuff[SERIAL_BUFF_SIZE];
+uint16_t  flashBuffIndex = 0;
+uint16_t imageCRC = 0;
 uint32_t flashAddr = 256;
 uint32_t imageSize = 0;
 
-uint8_t makeHex()
+void sendAck()
 {
-    uint8_t ch = UART_read();
+    UART_write( ack[0] );
+    UART_write( ack[1] );
+    UART_write( ack[2] );
+    UART_write( ack[3] );
+}
+
+uint8_t makeHex( uint8_t ch )
+{
     if( ch >= '0' && ch <= '9' )
         ch -= '0';
     else if( ch >= 'A' && ch <= 'F' )
@@ -25,23 +36,18 @@ uint8_t makeHex()
     return ch;
 }
 
-uint8_t getHex()
+uint8_t getHex( uint8_t *buff )
 {
-    uint8_t byte = makeHex();
+    uint8_t byte = makeHex( *buff );
     byte <<= 4;
-    byte |= makeHex();
+    byte |= makeHex( *( buff + 1 ) );
     return byte;
 }
 
 void writeToFlash( uint16_t len )
 {
     if( ( flashAddr % 256 ) == 0 ) FLASH_erasePage( flashAddr );
-
-    if( usePong )
-        FLASH_writeBytes( flashAddr, ping, len );
-    else
-        FLASH_writeBytes( flashAddr, pong, len );
-
+    FLASH_writeBytes( flashAddr, flashBuff, len );
     flashAddr += len;
     imageSize += len;
 }
@@ -61,58 +67,108 @@ void burnFlashHeader()
     FLASH_writeBytes( 0, header, 12 );
 }
 
-void runSerialBootLoader()
+void runBootLoader( uint8_t hex )
 {
-    uint8_t type, len;
+    flashBuff[flashBuffIndex++] = hex;
+    if( flashBuffIndex == 256 ) {
+        writeToFlash( flashBuffIndex );
+        flashBuffIndex = 0;
+    }
+}
 
-    flashAddr = 256;
-    imageSize = 0;
+void serialFormatToAMRFormat()
+{
+    char c = serialBuff[numSerialLengthBytes + 2];
+    if( serialBuff[numSerialLengthBytes + 1] == RF_WS_OTA_KEY ) {
+        switch( c ) {
+            case ':':
+                for( uint8_t i = 0; i < ( serialLength - 3 ); i += 2 )
+                    runBootLoader(
+                        getHex( &serialBuff[numSerialLengthBytes + 3 + i] ) );
 
+                sendAck();
+                break;
+            case '?':
+                if( serialBuff[numSerialLengthBytes + 3] == 'I' ) {
+                    char *crc =
+                        (char *)malloc( sizeof( char ) * ( serialLength - 3 ) );
+                    memcpy( crc, &serialBuff[numSerialLengthBytes + 4],
+                            ( serialLength - 4 ) );
+                    crc[serialLength - 4] = 0;
+
+                    uint16_t crcInt = atoi( crc );
+                    imageCRC = 0;
+                    imageCRC |= crcInt & 0xFF;
+                    imageCRC |= ( crcInt >> 8 ) & 0xFF;
+                    free( crc );
+
+                    sendAck();
+                }
+                else if( serialBuff[numSerialLengthBytes + 3] == 'E' ) {
+                    writeToFlash( flashBuffIndex );
+                    flashBuffIndex = 0;
+                    flashComplete = 1;
+                }
+                break;
+        }
+    }
+    else if( serialBuff[numSerialLengthBytes + 1] == RF_WS_CMD_KEY ) {
+        if( serialBuff[numSerialLengthBytes + 2] == '7' )
+            sendAck();
+    }
+}
+
+void getSerialLength()
+{
+    // Find out how many length bytes there are
+    for( uint8_t i = 0; i < serialIndex; i++ ) {
+        if( serialBuff[i] != RF_WS_INBOUND_DELIMITER )
+            numSerialLengthBytes++;
+        else
+            break;
+    }
+
+    // Copy the length bytes over to integer format
+    char *str = (char *)malloc( sizeof( char ) * ( numSerialLengthBytes + 1 ) );
+    memcpy( str, serialBuff, numSerialLengthBytes );
+    str[numSerialLengthBytes] = 0;
+    serialLength = atoi( str );
+    free( str );
+
+    serialLengthisSet = 1;
+}
+
+void serialParser( uint8_t c )
+{
+    if( serialIndex < SERIAL_BUFF_SIZE ) {
+        serialBuff[serialIndex++] = c;
+
+        // If we have enough bytes then we should get the length
+        if( serialIndex > 1 && !serialLengthisSet ) getSerialLength();
+
+        // Once we have the entire command, parse it and reset the state machine
+        if( serialLengthisSet &&
+            ( ( serialIndex - numSerialLengthBytes ) >= serialLength ) ) {
+            serialFormatToAMRFormat();
+            serialIndex = 0;
+            numSerialLengthBytes = 0;
+            serialLengthisSet = 0;
+        }
+    }
+    else {
+        serialIndex = 0;
+        serialLengthisSet = 0;
+    }
+}
+
+void serialConsole()
+{
     while( 1 ) {
-        type = getHex();
-        SEND_ACK;
-        len = getHex();
-        SEND_ACK;
-
-        if( type == 1 ) {
-            usePong ^= 1;
-            writeToFlash( ndx );
+        uint8_t ch = UART_read();
+        serialParser( ch );
+        if( flashComplete ) {
             burnFlashHeader();
             checkFlashImage();
-        }
-        else if( type == 0 ) {
-            for( ; len > 0; len-- ) {
-                // Check for a full buffer
-                if( ndx == 256 ) {
-                    ndx = 0;
-                    usePong ^= 1;
-                    writeTriggered = 1;
-                }
-
-                // Fill the ping or pong buffer
-                if( usePong )
-                    pong[ndx++] = getHex();
-                else
-                    ping[ndx++] = getHex();
-            }
-
-            // Write to flash memory
-            if( writeTriggered ) {
-                writeTriggered = 0;
-                writeToFlash( 256 );
-            }
-
-            SEND_ACK;
-        }
-        else if( type == 3 ) {
-            uint8_t i;
-
-            // Record type 3 has the code segment and program counter to load
-            // however that segment of code is already located at the boot
-            // address so just eat the bytes
-            for( i = 0; i < 4; i++ ) getHex();
-
-            SEND_ACK;
         }
     }
 }
--- serial.h
+++ serial.h
@@ -3,6 +3,11 @@
 
 #include <hardware.h>
 
-void runSerialBootLoader();
+#define SERIAL_BUFF_SIZE 256
+#define RF_WS_INBOUND_DELIMITER '!'
+#define RF_WS_OTA_KEY 'O'
+#define RF_WS_CMD_KEY 'C'
+
+void serialConsole();
 
 #endif /* SERIAL_H_ */
